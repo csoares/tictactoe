a5fd0c55764caafc6a83adb428934cec4cbd1054
Christophe Soares <christophe.p.soares@mindera.com>
2023-04-28 15:10:55 +0100

    fix filename

--- src/components/Game/index.js
+++ src/components/Game/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Board from "../Board/index.";
+import Board from "../Board";
 import { GameContainer, GameInfo, OrderedList } from "./style";
 
 const Game = () => {
